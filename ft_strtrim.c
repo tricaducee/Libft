4288a062b024db2451c53c38b0cf8c5bbb222ee0
Hermes Rolle <hrolle@student.42lausanne.ch>
2021-11-15 17:11:17 +0100

    ft_strtrim v2

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hrolle <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 17:57:09 by hrolle            #+#    #+#             */
-/*   Updated: 2021/11/15 01:56:31 by hrolle           ###   ########.fr       */
+/*   Updated: 2021/11/15 13:49:50 by hrolle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,18 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	i;
-	unsigned int	j;
 	unsigned int	in;
 	char			*ret;
 
 	i = 0;
-	while (s1[i] && set[j])
-	{
-		j = 0;
-		while (set[j] && set[j++] != s1[i])
-		if (set[j])
-			i++;
-	}
-	in = i;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	in = i - 1;
 	while (s1[i])
 		i++;
-	while (s1[i] && set[j])
-	{
-		j = 0;
-		while (set[j] && set[j++] != s1[i])
-		if (set[j])
-			i--;
-	}
-	i -= in;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i--;
+	i -= in - 1;
 	ret = malloc((i + 1) * sizeof(char));
 	if (!ret)
 		return (0);
