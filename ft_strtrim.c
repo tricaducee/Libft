8b845dd4d39f6a9da2d3cd882051ec681d384d3a
Hermes Rolle <hrolle@student.42lausanne.ch>
2021-11-24 02:12:47 +0100

    final ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hrolle <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 17:57:09 by hrolle            #+#    #+#             */
-/*   Updated: 2021/11/15 13:49:50 by hrolle           ###   ########.fr       */
+/*   Updated: 2021/11/23 20:49:54 by hrolle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char			*ret;
 
 	i = 0;
+	if (!s1 || !set)
+		return (0);
+	if (!*s1)
+		return (ft_strdup(""));
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	in = i - 1;
+	if (!s1[i])
+		return (ft_strdup(""));
+	in = i;
 	while (s1[i])
 		i++;
+	i--;
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i--;
 	i -= in - 1;
@@ -33,3 +40,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(ret, s1 + in, i + 1);
 	return (ret);
 }
+/*
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", ft_strtrim(argv[1], argv[2]));
+	return (0);
+}*/
