c5e7533dc611c85dac442644fa32b87232b4beac
Hermes Rolle <hrolle@student.42lausanne.ch>
2022-01-01 19:29:56 +0100

    Final + bonus

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,57 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrolle <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/31 18:16:30 by hrolle            #+#    #+#             */
-/*   Updated: 2022/01/01 00:43:09 by hrolle           ###   ########.fr       */
+/*   Created: 2022/01/01 17:48:14 by hrolle            #+#    #+#             */
+/*   Updated: 2022/01/01 18:28:51 by hrolle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
 	t_list	*start;
 
-	if (!lst || !f || !del)
+	if (!lst)
 		return (NULL);
 	newlst = ft_lstnew(f(lst->content));
 	if (!newlst)
 		return (NULL);
 	start = newlst;
-	lst = lst->next;
-	while (lst)
+	while (lst->next)
 	{
+		lst = lst->next;
 		newlst->next = ft_lstnew(f(lst->content));
 		if (!newlst->next)
 		{
 			ft_lstclear(&start, del);
 			return (NULL);
 		}
-		lst = lst->next;
 		newlst = newlst->next;
 	}
 	newlst = NULL;
 	return (start);
-}
-*/
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list *new_lst;
-	t_list *elem;
-
-	if (!lst)
-		return (0);
-	new_lst = 0;
-	while (lst)
-	{
-		if (!(elem = ft_lstnew(f(lst->content))))
-		{
-			ft_lstclear(&new_lst, del);
-			return (0);
-		}
-		ft_lstadd_back(&new_lst, elem);
-		lst = lst->next;
-	}
-	return (new_lst);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,41 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrolle <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/04 08:53:04 by hrolle            #+#    #+#             */
-/*   Updated: 2021/12/31 20:22:54 by hrolle           ###   ########.fr       */
+/*   Created: 2022/01/01 18:58:07 by hrolle            #+#    #+#             */
+/*   Updated: 2022/01/01 19:11:33 by hrolle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	while (dst[i])
-		i++;
-	if (i > dstsize)
-		return (dstsize + ft_strlen(src));
-	while (src[j] && i < dstsize - 1)
-		dst[i++] = src[j++];
-	dst[i] = 0;
-	while (src[j++])
-		i++;
-	return (i);
-}
-*/
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (dst[i] && i < dstsize)
+	while (i < dstsize && dst[i])
 		i++;
 	j = i;
 	while (src[i - j] && i + 1 < dstsize)
@@ -47,7 +26,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[i] = src[i - j];
 		i++;
 	}
-	if (j < dstsize)
+	if (i < dstsize)
 		dst[i] = 0;
 	return (j + ft_strlen(src));
 }
